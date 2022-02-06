9c6f132d34143cddddb13a817a67ba81c83a0ceb
tomisargiottob <tomisargiottob@hotmail.com>
2022-02-06 12:15:40 +0100

    refactor(logger): change logger from multistream to pino single stream

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,35 +1,19 @@
-const childProcess = require('child_process');
-const stream = require('stream');
-const pinoms = require('pino-multi-stream');
+const config = require('config');
+const pino = require('pino');
 
-// Environment variables
-const cwd = process.cwd();
-const { env } = process;
-const logPath = `${cwd}/log`;
+const formatters = {
+  bindings() {
+    return {};
+  },
+  level(label) {
+    return { level: label };
+  },
+};
 
-// Create a stream where the logs will be written
-const logThrough = new stream.PassThrough();
-const prettyStream = pinoms.prettyStream({
-  prettyPrint:
-    {
-      colorize: true,
-      translateTime: 'SYS:standard',
-      ignore: 'hostname,pid', // add 'time' to remove timestamp
-    },
+const logger = pino({
+  timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`,
+  formatters,
+  level: (config.logger && config.logger.level) || 'debug',
 });
-const streams = [
-  { stream: logThrough },
-  { stream: prettyStream },
-];
-
-const logger = pinoms(pinoms.multistream(streams));
-
-const child = childProcess.spawn(process.execPath, [
-  require.resolve('pino-tee'),
-  'error', `${logPath}/error.log`,
-  'fatal', `${logPath}/fatal.log`,
-], { cwd, env });
-
-logThrough.pipe(child.stdin);
 
 module.exports = logger;
